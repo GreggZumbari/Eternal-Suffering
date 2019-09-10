cb02ad8b4d293642bf1694132cb4cc5f37fb0e0a
Gregg H <50878128+GreggZumbari@users.noreply.github.com>
2019-09-10 10:49:14 -0700

    End of 9/10/2019

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-  char[] input;
-  cin >> input;
-  for (int i = 0; i < input.length; i++) {
-    cout << input[i];
+  int input;
+
+  int real;
+  int max = 100;
+
+  srand (time(NULL));
+  real = rand()%100;
+  std::cout << real;
+  /*
+  std::cout << "Hello sir/ma'am, please enter a number between 1 and 100: ";
+  while (!input <= 100 && !input >= 0) {
+    try {
+      
+    } catch (int ex) {
+      std::cout << "That input is invalid, please enter a number between 1 and 100: ";
+    }
   }
+  */
   return 0;
 }
