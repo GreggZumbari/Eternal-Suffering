36a222e8953e7d600eb0400fb605b99212473d15
Gregg H <50878128+GreggZumbari@users.noreply.github.com>
2019-09-12 10:49:14 -0700

    End of 9/12/2019

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -7,23 +7,40 @@ using namespace std;
 
 int main()
 {
-  int input;
-
   int real;
+  int tries;
+
+  int input = NULL;
   int max = 100;
 
-  srand (time(NULL));
-  real = rand()%100;
-  std::cout << real;
-  /*
-  std::cout << "Hello sir/ma'am, please enter a number between 1 and 100: ";
-  while (!input <= 100 && !input >= 0) {
-    try {
-      
-    } catch (int ex) {
-      std::cout << "That input is invalid, please enter a number between 1 and 100: ";
+  //Generating the random number
+  srand(time(NULL));
+  real = rand() % max;
+
+  //Start the game
+  std::cout << "Hello sir/ma'am, please enter a number between 1 and " << max << ": " << endl;
+
+  //Ask for number input
+  try {
+    std::cin >> input;
+  }
+  catch (int ex)
+  {}
+
+  while (true) {
+    //If input is invalid, keep trying until the stupid user finally gives you a correct input
+    while (input == NULL || input > 100 || input < 0) {
+      std::cout << "That input is invalid, please enter a number between 1 and " << max << ":" << endl;
+      try {
+	std::cin >> input;
+      }
+      catch (int ex) {}
+    }
+
+    //Compare if their guess to the number, and return an output
+    if (input > real) {
+      //Too high
     }
   }
-  */
   return 0;
 }
