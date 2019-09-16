9b2f381e1827e3f084aca78c7fcf26a59c98a389
Gregg H <50878128+GreggZumbari@users.noreply.github.com>
2019-09-16 10:01:26 -0700

    COMPLETE!!!!! FOR REAL THIS TIME!!!!

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -7,39 +7,73 @@ using namespace std;
 
 int main()
 {
+  
+  bool playing = true;
+
+  char consent = 'y';
+
   int real;
+  int max;
+  int input;
   int tries;
 
-  int input = NULL;
-  int max = 100;
+  while (consent == 'y') {
 
-  //Generating the random number
-  srand(time(NULL));
-  real = rand() % max;
+    playing = true;
+    max = 100;
+    input = max + 69;
+    tries = 0;
+  
+    //Generating the random number
+    srand(time(NULL));
+    real = rand() % max;
 
-  //Start the game
-  std::cout << "Hello sir/ma'am, please enter a number between 1 and " << max << ": " << endl;
+    //Start the game
+    std::cout << "...Hello sir/ma'am, please enter a number between 1 and " << max << ": " << endl;
 
-  //Ask for number input
-  try {
-    std::cin >> input;
-  }
-  catch (int ex)
-  {}
-
-  while (true) {
-    //If input is invalid, keep trying until the stupid user finally gives you a correct input
-    while (input == NULL || input > 100 || input < 0) {
-      std::cout << "That input is invalid, please enter a number between 1 and " << max << ":" << endl;
+    while (playing) {
+  
+      //Ask for number input                                                        
       try {
-	std::cin >> input;
+        std::cin >> input;
       }
       catch (int ex) {}
-    }
 
-    //Compare if their guess to the number, and return an output
-    if (input > real) {
-      //Too high
+      //If input is invalid, keep trying until the stupid user finally gives you a correct input
+      while (input > max || input < 0) {
+        std::cout << "That input is invalid, please enter a number between 1 and " << max << ":" << endl;
+        try {
+          std::cin >> input;
+        }
+        catch (int ex) {}
+      }
+
+      //Compare their guess to the number, and return an output
+      if (input > real) {
+        std::cout << "Too high, guess again: " << endl;
+        tries++;
+      }
+      if (input < real) {
+        std::cout << "Too low, guess again: " << endl;
+        tries++;
+      }
+      //If they guessed the right number
+      if (input == real) {
+	playing = false;
+	consent = '?';
+        std::cout << "Congratulations, you have successfully guessed the number " << real << " in only " << tries << " tries." << endl;
+        if (tries > 7)
+	  std::cout << "Silly human." << endl;
+	//Ask if they would like to play again
+	while (consent != 'y' && consent != 'n') {
+	  std::cout << "Would you like to play again? (y/n)" << endl;
+	  try {
+	    std::cin >> consent;
+	  } 
+	  catch (int ex) {}
+	}
+      }
+      input = max + 69;
     }
   }
   return 0;
